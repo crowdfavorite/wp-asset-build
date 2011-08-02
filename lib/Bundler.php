0d76fcff08f3d3e39261bdaa34e20446a71dd3c7
gbrander <gbrander@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-08-02 16:53:24 +0000

    Removed large chunks of commented-out code. Added comments to methods. Checking to make sure item pushed with ->push is indeed a Bundle.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/asset-build-script/trunk@38486 546ca710-db30-0410-8887-d9dd8e66aff0

--- lib/Bundler.php
+++ lib/Bundler.php
@@ -1,43 +1,59 @@
 <?php
-
+/**
+ * Combine files together. Hooray!
+ */
 class Bundler {
+	/**
+	 * Keeps a globally-accessible list of bundles that need to be built
+	 */
 	public static $build_profiles = array();
-	
-	// Properties
 	protected $asset_path_prefix;
 	protected $bundles = array();
-	protected $enabled;
 	
-	// Constructor
-	protected function __construct($my_path_prefix) {
-		$this->enabled = true;
-		$this->asset_path_prefix = self::add_trailing_slash($my_path_prefix);
+	/**
+	 * Constructor does not have public access - use factory to initialize
+	 */ 
+	protected function __construct($asset_path_prefix) {
+		$this->asset_path_prefix = self::add_trailing_slash($asset_path_prefix);
 	}
 	
-	// Factory
-	public function create($my_path_prefix='') {
-		$ins = new Bundler($my_path_prefix);
+	/**
+	 * Factory method. Use this to create a new Bundler.
+	 */
+	public function create($asset_path_prefix = '') {
+		$ins = new Bundler($asset_path_prefix);
+		
+		// Add to $build_profiles so we know what needs to be built
 		self::$build_profiles[] = $ins;
 		
 		return $ins;
 	}
 	
+	/**
+	 * Add a configured Bundle object to the Bundler
+	 */
 	public function push($bundle) {
+		if (!($bundle instanceof Bundle)) {
+			throw new Exception("You can only push objects that are an instanceof Bundler", 1);
+		}
 		$this->bundles[] = $bundle;
 	}
 	
 	public function get_bundles() {
 		return $this->bundles;
 	}
+	
 	public function get_path_prefix() {
 		return $asset_path_prefix;
 	}
+	
 	public function get_bundled_paths() {
 		foreach ($this->bundles as $bundle) {
 			$my_array[$bundle->get_bundle_key] = $this->get_full_path($bundle->get_bundled_path());
 		}
 		return $my_array;
 	}
+	
 	public function get_original_paths() {
 		foreach ($this->bundles as $bundle) {
 			foreach($bundle->get_original_paths() as $original_bundle_key => $original_bundle_path) {
@@ -47,10 +63,14 @@ class Bundler {
 		return $my_array;
 	}
 	
-	// Utility Functions
+	/**
+	 * We're using this instead of trailingslashit, because Bundler is designed to work
+	 * outside of WordPress
+	 */
 	private static function add_trailing_slash($my_path) {
 		return (substr($my_path, -1)!='/')?$my_path.'/':$my_path;
 	}
+	
 	public function get_full_path($my_path_suffix) {
 		return $this->asset_path_prefix . $my_path_suffix;
 	}
@@ -69,6 +89,7 @@ class Bundle {
 		$this->bundle_items[$bundle_item_key] = new BundleItem($bundle_item_key, $my_path, $my_replacements = array());
 		return $this;
 	}
+	
 	public function get_bundle_key() {
 		$keys = array();
 		foreach($this->bundle_items as $bundle_item) {
@@ -76,12 +97,15 @@ class Bundle {
 		}
 		return implode('-', $key);
 	}
+	
 	public function get_bundle_items() {
 		return $this->bundle_items;
 	}
+	
 	public function get_bundled_path() {
 		return $this->output_path;
 	}
+	
 	public function get_original_paths() {
 		foreach($this->bundle_items as $bundle_item) {
 			$my_array[$bundle_item->get_key()] = $bundle_item->get_path();
@@ -89,9 +113,11 @@ class Bundle {
 		
 		return $my_array;
 	}
+	
 	public function set_language($language) {
 		return $this->set_meta('language', $language);
 	}
+	
 	public function get_language() {
 		return($this->meta['language'])?$this->meta['language']:'';
 	}
@@ -100,6 +126,7 @@ class Bundle {
 		$this->meta[$key] = $value;
 		return $this;
 	}
+	
 	public function get_meta($key) {
 		return $this->meta[$key];
 	}
@@ -123,88 +150,13 @@ class BundleItem {
 	public function get_key() {
 		return $this->key;
 	}
+	
 	public function get_path() {
 		return $this->path;
 	}
+	
 	public function get_replacements() {
 		return $this->replacements;
 	}
 }
-
-
-
-// /* Combine files together. Hooray! */
-// class Bundler {
-// 	public $asset_path;
-// 	public $bundles = array();
-// 	protected static $build_profiles;
-// 	
-// 	public static function create($asset_path_prefix = '') {
-// 		$ins = new Bundler($asset_path_prefix);
-// 		self::$build_profiles[] = $ins;
-// 		return $ins;
-// 	}
-// 	
-// 	/**
-// 	 * @param string $asset_path_prefix a path that asset paths should be prefixed with in order to find them on the file system.
-// 	 */
-// 	protected function __construct($asset_path_prefix = '') {
-// 		$this->asset_path = $asset_path_prefix;
-// 	}
-// 	
-// 	/**
-// 	 * We need an easy way to send an instances of Bundler to the build script.
-// 	 * This function, when run, will set the current instance as the instance to be built by the build script.
-// 	 */
-// 	public function add_to_build_profiles() {
-// 		self::$build_profiles[] = $this;
-// 	}
-// 	/**
-// 	 * Static function used by the build script to get bundles to build.
-// 	 */
-// 	public static function get_build_profiles() {
-// 		return self::$build_profiles;
-// 	}
-// 
-// 	/**
-// 	 * Define a file bundle
-// 	 * @param string $built_file the file name of the resulting built file
-// 	 * @param array $files the array of file names you want to combine into the built file
-// 	 */
-// 	public function define($bundle, $built_file) {
-// 		$this->bundles[$bundle]['build'] = $built_file;
-// 	}
-// 	
-// 	/**
-// 	 * Add a file to a bundle
-// 	 * @param string $bundle Bundle key
-// 	 * @param string $file File URL
-// 	 * @param array $replacements string replacements for the file
-// 	 */
-// 	public function add_to($bundle, $file, $replacements = array()) {
-// 		$this->bundles[$bundle]['src'][$file]['replacements'] = $replacements;
-// 	}
-// 	
-// 	public function get_bundled_file($bundle) {
-// 		$bundle = $this->bundles[$bundle];
-// 		if (!isset($bundle)) {
-// 			throw new Exception('Bundle "'.$bundle.'" doesn\'t exist', 1);
-// 		}
-// 		return $bundle['build'];
-// 	}
-// 	
-// 	public function get_original_files($bundle = '') {
-// 		$keys = array();
-// 		$bundle = $this->bundles[$bundle];
-// 		if (!isset($bundle)) {
-// 			throw new Exception('Uh-oh! Bundle "'.$bundle.'" doesn\'t exist', 1);
-// 		}
-// 
-// 		foreach ($bundle['src'] as $file => $info) {
-// 			$keys[] = $file;
-// 		}
-// 
-// 		return $keys;
-// 	}
-// }
 ?>
